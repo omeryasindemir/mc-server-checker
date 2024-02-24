6b9aeb3db5b32e3dd64d0a1603c9e230666fb60a
omeryasindemir <omeryasindemir66@gmail.com>
2024-02-24 13:05:17 +0300

    commit

--- src/pages/ServerCheck.jsx
+++ src/pages/ServerCheck.jsx
@@ -19,20 +19,22 @@ const ServerCheck = () => {
             <div className="box">
                 <h2>MC Server Checker by felX</h2>
                 <form onSubmit={handleCheck}>
-                    <input type="text" name="server_ip" id="server_ip" required onChange={e => setserverIP(e.target.value)} placeholder='ServerIP'/>
+                    <input type="text" name="server_ip" id="server_ip" required onChange={e => setserverIP(e.target.value)} placeholder='ServerIP' />
                     <button type='submit'>Check</button>
                 </form>
 
                 <ul>
-                    <li>Hostname : {Data.hostname}</li>
-                    <li>IP : {Data.ip}</li>
-                    <li>Online / Offline : {Data.online == true && <span>Online</span>} {Data.online == false && <span>Offline</span>}</li>
-                    <li>Max Player Limit : {Data?.players?.max}</li>
-                    <li>Online Players : {Data?.players?.online}</li>
-                    <li>Protocol : {Data?.protocol?.name}</li>
-                    <li>Version : {Data.version}</li>
+                    <li><div>Hostname</div><div>{Data.hostname}</div></li>
+                    <li><div>IP</div><div>{Data.ip}</div></li>
+                    <li><div>Limit</div><div>{Data?.players?.max}</div></li>
+                    <li><div>Online</div><div>{Data?.players?.online}</div></li>
+                    <li className='online'>{Data.online == true && <span>Online</span>} {Data.online == false && <span>Offline</span>}</li>
+                    
+                    <li>{Data.version}</li>
 
                 </ul>
+
+
             </div>
         </React.Fragment>
     )
